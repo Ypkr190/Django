fed2cbc9de5c763de7b445e190c34cd4635eaf43
Ypkr190 <ypkr190@gmail.com>
2024-06-18 12:34:59 +0530

    small change

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -40,7 +40,6 @@ def registration_view(request):
             
             data['token'] = token
             
-            # changed
             # Email Notification after registration
             """"
             subject = "Thanking for registration to our application"
